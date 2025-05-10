49a0215edbc8ad983b53d4e9ff5bbdc24188f338
butterfly301 <157933717+butterfly301@users.noreply.github.com>
2025-05-11 07:51:36 +0800

    主菜单界面

---
+++ Assets/FairyOnTheTree/Scripts/UI/Menu.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup canvasGroup;
+    [SerializeField] private float fadeDuration = 2f;
+    [SerializeField] private string sceneToLoad;
+    public void ClickStart()
+    {
+        canvasGroup.alpha = 0f;
+        
+        // 使用DOTween进行渐变
+        canvasGroup.DOFade(1f, fadeDuration)
+            .OnComplete(() => SceneManager.LoadScene(sceneToLoad));
+    }
+
+    public void ClickExit()
+    {
+        Application.Quit();
+    }
+}
